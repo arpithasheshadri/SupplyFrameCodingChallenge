c7cd9715103b1fbf6d6954060b9dfef1b3ea1fff
Arpitha Sheshadri Bhat <144745793+arpithasheshadri@users.noreply.github.com>
2024-03-30 01:11:05 -0400

    modified dashboard to display cards with background

--- backend/services/weather-service.js
+++ backend/services/weather-service.js
@@ -4,7 +4,7 @@ const API_KEY = "6fa67728bcac232e903ecd8614c7475f";
 
 const weatherByPlace = async (lat,lon) => {
   const response = await axios.get(
-    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,relative_humidity_2m,precipitation,rain,snowfall,wind_speed_10m,uv_index,is_day&daily=sunrise,sunset&timezone=GMT&forecast_days=1`
+    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,relative_humidity_2m,precipitation,rain,snowfall,wind_speed_10m,uv_index,is_day&daily=sunrise,sunset&timezone=auto&forecast_days=1`
   );
   
   return response.data;
